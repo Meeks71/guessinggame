a383f5249d32af1c96f310577a6b94f93154957e
Marcus Meekins <meeks_mha@yahoo.com>
2022-07-10 13:27:43 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+//guessing game
+//set a number between 1 and 10
+//let guessNumber = 7
+//const number = Math.floor(Math.random() * 10) +1;
+
+ //prompt the user to make a guess
+ //prompt("Guess a number from 1 to 10");
+
+//game logic and Loop
+  //if(number < guessNumber){
+  //  console.log('Your guess is too low!')
+   //}else(number > guessNumber) {
+  //  console.log('Your guess is too high!');
+  // }else{
+   //     console.log('Winner!')};
+
+       
